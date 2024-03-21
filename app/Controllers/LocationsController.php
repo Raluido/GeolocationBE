929b556c20a2d1ac97ab883ff1e42d44f77b77ea
raluido <raluido@gmail.com>
2024-03-21 14:33:57 +0000

    trying to recibe json

--- app/Controllers/LocationsController.php
+++ app/Controllers/LocationsController.php
@@ -7,6 +7,7 @@ use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\HTTP\Request;
 use App\Models\Location;
+use PgSql\Connection;
 
 class LocationsController extends ResourceController
 {
@@ -22,28 +23,20 @@ class LocationsController extends ResourceController
     public function create()
     {
         $model = new Location();
-        $locations  = $model->save($this->request->getJSON());
+        // $locations  = $model->save($this->request->getJSON());
 
-        log_message('debug', $locations);
+        log_message('debug', $this->request->getJSON());
 
         die();
 
         // $geojson = file_get_contents("o/1.geojson");
-        // //Translate that into JSON-compliant array of features
         // $features = json_decode($geojson, TRUE)->features;
-        // //Connect to your database
         // $connection = pg_connect(...);
-        // //Iterate over features within FeatureCollection
         // foreach ($features as $feature) {
-        //     //Extract necessary attributes
         //     $layer = $feature->properties["Layer"];
         //     $subClasses = $feature->properties["SubClasses"];
-
-        //     //Extract geometry attribute as a string
         //     $geomJson = json_encode($feature->geometry);
-        //     //Make up SQL-query
         //     $sql = "insert into mytable (layer, subclasses, ...., geom) values ('" . $layer . "','" . $subClasses . "',..., st_geomfromgeojson('" . $geomJson . "')";
-        //     //Execute the query
         //     pg_query($connection, $sql);
         // }
 
